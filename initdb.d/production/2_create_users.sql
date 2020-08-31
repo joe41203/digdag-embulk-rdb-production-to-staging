ede84bd763325f2ba87be7424fdf2969d7192510
joe <kinjoyuji@gmail.com>
2020-08-31 22:18:32 +0900

    大量のデータをinsertしてみた

--- initdb.d/production/2_create_users.sql
+++ initdb.d/production/2_create_users.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-  id           INT(10),
+  id           INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name      VARCHAR(40),
   email     VARCHAR(120),
   nickname     VARCHAR(40)
